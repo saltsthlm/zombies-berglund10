8deb59b7de99e108b14e10b3764d184534cb05b8
Berglund Anton <anton.berglund@iths.se>
2024-09-11 13:14:10 +0200

    5th test

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -76,7 +76,13 @@ test("one-roomer becomes full when a zombie is added", () => {
 })
 //test.skip("one-roomer becomes full when a zombie is added", () => {});
 
-test.skip("two-roomer is not full when a zombie is added", () => { });
+test("two-roomer is not full when a zombie is added", () => {
+    const room = createRoom(2);
+    room.addZombie("bosse");
+    const isRoomFull = room.isFull();
+    ok(!isRoomFull);
+})
+//test.skip("two-roomer is not full when a zombie is added", () => { });
 
 test.skip("second zombie consumes first zombie when added to a one-roomer", () => { });
 
